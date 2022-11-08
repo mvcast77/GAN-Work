6e286bb5195dad80ce41150ba472ba5a1905981d
mvcast77 <87681131+mvcast77@users.noreply.github.com>
2022-11-08 13:41:51 -0800

    Adding Functions File

---
+++ Functions.py
@@ -0,0 +1,30 @@
+# Discriminator
+# Convolution
+# Max Pool
+
+# Repeart as needed
+
+# Flatten
+# Dense Layer
+# Output
+
+
+
+# Random Generator
+# noise = np.array(random)
+
+
+# Generator
+# Dense Layer
+# Normalize Layer
+# 
+# Leaky ReLu
+# Reshape (explanation needed)
+# Convolution stuff (Deconvolution?)
+# Normalize again
+
+# Repeat several times I guess
+
+# Return image values
+
+# Image Viewer
